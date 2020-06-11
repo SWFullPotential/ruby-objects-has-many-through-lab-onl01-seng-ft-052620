f9ecc34f7eb98e2348e0b8811a889649528512de
Samantha Wabi <swfullpotential@gmail.com>
2020-06-11 19:51:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
-  def patient
+  def patients
     appointments.map {|appointment| appointment.patient}
   end
   
